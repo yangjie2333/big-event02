d9f4bf09d1f529931fa0e5068acb52dc526eb5ff
yangjie233 <1149600856@qq.com>
2020-10-27 20:25:54 +0800

    结束了美好的一天

---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+// 入口函数
+$(function() {
+    getUserInfo()
+})
+
+// 封装一个全局的获取登录用户信息的函数
+//后面需要调用
+function getUserInfo() {
+    // ajax获取用户信息
+    $.ajax({
+        type: "get",
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function(res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            // 获取用户信息成功渲染页面
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    // 优先渲染昵称,没有昵称渲染用户名
+    var name = user.nickname || user.username
+
+    // 渲染欢迎文本
+    $('.welcome').html('欢迎&nbsp;&nbsp;' + name)
+
+    // 判断用户是否有头像,有的话渲染用户自己的头像,没有渲染名字的首字母
+    if (user.user_pic) {
+        // 用户有头像
+        $('.layui-nav-img').show().attr('src', user.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        var first = name[0].toUpperCase()
+        $('.layui-nav-img').hide()
+        $('.text-avatar').show().html(first)
+    }
+}
